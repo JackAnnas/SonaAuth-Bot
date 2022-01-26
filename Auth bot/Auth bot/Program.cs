813d73bc21162b1816f5312880ddea5d96b60777
JackAnnas <98243617+JackAnnas@users.noreply.github.com>
2022-01-26 19:04:46 +1100

    added dm key
    added to the bot so when u gen a token it will automatically send it to the customer u @ on discord.

---
+++ Auth bot/Auth bot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+
+namespace Auth_Bot
+{
+    class Program
+    {
+       static void  Main()
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        async static  Task AsyncMain()
+        {
+            var Client = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "OTM1Nzg1OTU4NDkyNzY2MjI5.YfDslg.qMQppihIyGC_D0K1LAiykbPm8Nc",
+                Intents = DiscordIntents.AllUnprivileged,
+                TokenType = TokenType.Bot
+            });
+
+            new DiscordConfiguration()
+            {
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+            };
+
+            var commands = Client.UseCommandsNext( new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "." }
+            });
+
+            commands.RegisterCommands<DevPoints>();
+
+        await  Client.ConnectAsync();
+          await Task.Delay(-1);
+
+                }
+    }
+}
---
+++ Auth bot/Auth bot/commands/DevPoints.cs
@@ -0,0 +1,30 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Net;
+
+public class DevPoints : BaseCommandModule
+{
+   // example of the link https://sonafreelance.xyz/api/admin/?devkey=88b5f92e88369d8a26411a56f4f97ed2&type=add&expiry=1&mask=XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX&level=1&amount=1&format=text
+
+    [Command("Gen-Token")]
+    public async Task generatekey(CommandContext ctx, DiscordMember member,string expiry, string level)
+    {
+
+        var wc = new WebClient();
+
+        var key = wc.DownloadString($"https://sonafreelance.xyz/api/admin/?devkey=88b5f92e88369d8a26411a56f4f97ed2&type=add&expiry={expiry}&mask=XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX&level={level}&amount=1&format=text");
+
+
+        var embed = new DiscordEmbedBuilder()
+        {
+            Description = "thank you for purchasing here are your details & key\r\n" +
+            $"here is your key: {key}\r\n" +
+            $"expiry {expiry} day/days\r\n" + 
+            $"level: {level}"
+        };
+
+       await member.SendMessageAsync(embed);
+    }
+}
